e171c33c7da58d6d6da8856c20656d3120f88ff5
karimsaqer <kareemsaqer1@gmail.com>
2025-08-28 16:10:47 +0300

    commenting in tic tac toe game

--- TicTacToe/src/components/Log.jsx
+++ TicTacToe/src/components/Log.jsx
@@ -1,6 +1,7 @@
 export default function Log({turn}) {
     return (
-        <ol id="log">
+        // Render a list of turns taken in the game
+        <ol id="log"> 
             {turn.map((turn) => (
                 <li key={`${turn.square.row}${turn.square.col}`}>
                     Player {turn.player} placed on row {turn.square.row + 1}, column {turn.square.col + 1}
