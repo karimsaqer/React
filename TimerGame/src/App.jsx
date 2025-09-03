487002b85e2982cb2bc194d5de2fe15b194632bb
karimsaqer <kareemsaqer1@gmail.com>
2025-09-03 16:07:57 +0300

    Adding Header component

--- TimerGame/src/App.jsx
+++ TimerGame/src/App.jsx
@@ -1,8 +1,10 @@
+import Header from './components/Header.jsx';
 import Player from './components/Player.jsx';
 
 function App() {
   return (
     <>
+      <Header />
       <Player />
       <div id="challenges"></div>
     </>
