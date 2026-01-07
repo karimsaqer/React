8bdb78dcf9429dbe519bc9faa65e85352646fb47
karimsaqer <kareemsaqer1@gmail.com>
2026-01-07 19:57:28 +0300

    todo

---
+++ todo/src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+import SideBar from "./components/SideBar";
+
+function App() {
+  const [projectsState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  function handleAddProject(projectData) {
+    setProjectState((prevState) => {
+      const newProject = {
+        ...projectData,
+        id: Math.random(),
+      };
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  }
+
+  let content;
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject} />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+  return (
+    <main className="h-screen my-8 flex gap-8">
+      <SideBar
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+      />
+      {content}
+    </main>
+  );
+}
+
+export default App;
---
+++ todo/src/components/NewProject.jsx
@@ -0,0 +1,43 @@
+import Input from "./Input";
+import { useRef } from "react";
+export default function NewProject({onAdd}) {
+  const title = useRef();
+  const Description = useRef();
+  const dueDate = useRef();
+
+  function handleSave() {
+    const enteredTitle = title.current.value;
+    const enteredDescription = Description.current.value;
+    const entereddueDate = dueDate.current.value;
+
+    // .. validate
+    onAdd({
+        title:enteredTitle,
+        description: enteredDescription,
+        dueDate: entereddueDate,
+    });
+  }
+  return (
+    <div className="w-[35rem] mt-16">
+      <menu className="flex items-center justify-end gap-4 my-4">
+        <li>
+          <button className="text-stone-800 hover:text-stone-950">
+            Cancel
+          </button>
+        </li>
+        <li>
+          <button
+            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+            onClick={handleSave}>
+            Save
+          </button>
+        </li>
+      </menu>
+      <div>
+        <Input type="text" ref={title} label="Title" />
+        <Input ref={Description} label="Description" isTextArea />
+        <Input type="date" ref={dueDate} label="Due Date" />
+      </div>
+    </div>
+  );
+}
